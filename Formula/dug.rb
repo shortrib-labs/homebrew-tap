e8b982b110e89bf54b0b0b11238b4ede48da2e13
Chuck D'Antonio <chuck@replicated.com>
2026-04-18 03:56:48 -0400

    adds dug formula v0.2.1

---
+++ Formula/dug.rb
@@ -0,0 +1,28 @@
+class Dug < Formula
+  desc "macOS-native DNS lookup utility using the system resolver"
+  homepage "https://github.com/shortrib-labs/dug"
+  url "https://github.com/shortrib-labs/dug/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "8434e1b408aa5b8db507993ddbb5ec397557d612c4b15a0355aed97212203984"
+  license "MIT"
+
+  depends_on :macos
+  depends_on xcode: ["15.0", :build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/dug"
+    man1.install "dug.1"
+
+    output = Utils.safe_popen_read(bin/"dug", "--generate-completion-script", "zsh")
+    (zsh_completion/"_dug").write output
+    output = Utils.safe_popen_read(bin/"dug", "--generate-completion-script", "bash")
+    (bash_completion/"dug").write output
+    output = Utils.safe_popen_read(bin/"dug", "--generate-completion-script", "fish")
+    (fish_completion/"dug.fish").write output
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dug --version")
+    assert_match "ANSWER SECTION", shell_output("#{bin}/dug example.com")
+  end
+end
